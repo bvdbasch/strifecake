d2f6b29c6d42c1ca1e082e77cc8dc397fa760c69
Roel Bakker <roelbaka@gmail.com>
2014-08-22 17:11:22 +0200

    trying to find correct category function

--- posts.php
+++ posts.php
@@ -1,6 +1,10 @@
 <?php
     $backgroundArray = array('gael.jpg', 'lyrie.jpg', 'nestra.jpg', 'strife.jpg', 'tempra.jpg', 'vorbis.jpg' );
     $bgIMG = $backgroundArray[array_rand($backgroundArray)];
+
+    //Fetch category
+    //$category = end((explode('/', current_url())));
+    echo category_title();
 ?>
 <!DOCTYPE html>
 <head>
