6dd1dca744a942d8f20f99e057dbd07dd89fcfad
bvdbasch <bvdbasch@outlook.com>
2014-08-24 21:26:29 +0200

    Article category block finished
    Finished a article category block for the front page

--- Pages/Frontpage.php
+++ Pages/Frontpage.php
@@ -1,47 +1,23 @@
 <!-- CATEGORIES START HERE -->
-    <div id="LeftContainer">
-        <div id="CategoryLeft">News
-        <div id="BarBlue"></div>
+<div id="LeftContainer">
+    
+    <!-- NEWS CATEGORY-->
+    <div id="CategoryLeft">
+        <div id="CategoryHeader">NEWS</div>     <!-- Header -->    
+        <div id="CatBlue"></div>                <!-- Accent -->
+    
+        <!-- Content -->
         <div id="CategoryList">
-
-
-<!-- NEWS CATEGORY-->
-<?php 
-    if (has_posts()) {
-        while (posts()) {
+        <?php 
+            if (has_posts()) {
+                while (posts()) {
             if ( article_category() == 'news') { 
                 include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                    }
+                }
             }
-        }
-    }
-?>
-
-<!-- Pagination -->
-
-<!-- END NEWS CATEGORY-->
+        ?>
         </div>
-        </div>
-
-        <div id="CategoryRight">Learn
-            <div id="BarOrange"></div>
-                <div id="CategoryList">
-<?php 
-    if (has_posts()) {
-        while (posts()) {
-            if ( article_category() == 'learn') { 
-                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
-            }
-        }
-    }
-?>
-            </div>
-        </div>
-
     </div>
 
-    <div id="RightContainer">This is the sidebar. It would like some widgets</div>
-
-    </div>
-
-    <!-- END CATEGORIES -->
-
+</div>
--- Partials/FrontpageCategoryBlockHTML.php
+++ Partials/FrontpageCategoryBlockHTML.php
@@ -1,10 +1,27 @@
 <article>
-    <img src="<?php echo article_custom_field("thumb",theme_url('/assets/img/fallback.png'));?>">
-    <header>
-        <h1>
-            <a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a>
-        </h1>
-        <small><?php echo article_date(); ?></small>
+ <div id="ArticlePreview">
+     
+     <!-- Preview Image -->
+     <img class="frontpage" src="<?php echo article_custom_field("thumb",theme_url('Resources/IMG/fallback.png'));?>">
+    
+     <header>
+        <!-- Article title -->
+        <h1><a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a></h1>
+
+        <!-- Article subtitle -->
+        <h2><em>Posted by: <?php echo article_author();?> on <time datetime="<?php echo date(DATE_W3C, article_time());?>"><?php echo article_date();?></time></em></h2>
+
+        <!-- End article title -->
+    
     </header>
-    <p><?php echo article_description(); ?></p>
+    
+    <!-- Article description -->
+
+    <p><?php echo substr(article_description(),0,100); ?><a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"> ...</a></p>
+     
+     
+     
+
+    <!-- End article description -->
+</div>
 </article>
