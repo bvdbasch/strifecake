7f0007d3057698781a72ed87d8e128a39320c6b8
bvdbasch <bvdbasch@outlook.com>
2014-08-24 22:13:50 +0200

    Finished front page article index
    All categories added to front page

--- Pages/Frontpage.php
+++ Pages/Frontpage.php
@@ -3,8 +3,46 @@
     
     <!-- NEWS CATEGORY-->
     <div id="CategoryLeft">
-        <div id="CategoryHeader">NEWS</div>     <!-- Header -->    
-        <div id="CatBlue"></div>                <!-- Accent -->
+        <div id="CategoryHeader">NEWS</div> <!-- Header -->    
+        <div id="CatGreen"></div>           <!-- Accent -->
+    
+        <!-- Content -->
+        <div id="CategoryList">
+        <?php 
+            if (has_posts()) {
+                while (posts()) {
+            if ( article_category() == 'news') { 
+                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                    }
+                }
+            }
+        ?>
+        </div>
+    </div>
+    
+    <!-- LEARN CATEGORY-->
+    <div id="CategoryRight">
+        <div id="CategoryHeader">LEARN</div>    <!-- Header -->    
+        <div id="CatOrange"></div>              <!-- Accent -->
+    
+        <!-- Content -->
+        <div id="CategoryList">
+        <?php 
+            if (has_posts()) {
+                while (posts()) {
+            if ( article_category() == 'news') { 
+                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                    }
+                }
+            }
+        ?>
+        </div>
+    </div>
+    
+    <!-- COMPETITIVE CATEGORY-->
+    <div id="CategoryLeft">
+        <div id="CategoryHeader">COMPETITIVE</div>  <!-- Header -->    
+        <div id="CatPurple"></div>                  <!-- Accent -->
     
         <!-- Content -->
         <div id="CategoryList">
@@ -20,4 +58,41 @@
         </div>
     </div>
 
+    <!-- MEDIA CATEGORY-->
+    <div id="CategoryRight">
+        <div id="CategoryHeader">MEDIA</div>   <!-- Header -->    
+        <div id="CatRed"></div>                <!-- Accent -->
+    
+        <!-- Content -->
+        <div id="CategoryList">
+        <?php 
+            if (has_posts()) {
+                while (posts()) {
+            if ( article_category() == 'news') { 
+                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                    }
+                }
+            }
+        ?>
+        </div>
+    </div>
+    
+    <!-- BLOG CATEGORY-->
+    <div id="CategoryLeft">
+        <div id="CategoryHeader">BLOG</div>   <!-- Header -->    
+        <div id="CatGray"></div>              <!-- Accent -->
+    
+        <!-- Content -->
+        <div id="CategoryList">
+        <?php 
+            if (has_posts()) {
+                while (posts()) {
+            if ( article_category() == 'news') { 
+                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                    }
+                }
+            }
+        ?>
+        </div>
+    </div>
 </div>
