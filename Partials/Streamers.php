3420a48d7724d6d44e0e97c467f3d539e254bf89
Roel Bakker <roelbaka@gmail.com>
2014-08-24 21:28:54 +0200

    Added Streamer Widget

--- Partials/Streamers.php
+++ Partials/Streamers.php
@@ -1,11 +1,32 @@
-        <h2 class="section-head">STREAMERS</h2>
-        <div class="content">
-			<iframe src="http://streambadge.com/twitch/splcast/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-			<iframe src="http://streambadge.com/twitch/leeleethebunny/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-			<iframe src="http://streambadge.com/twitch/ryzentv/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-			<iframe src="http://streambadge.com/twitch/mfsplash/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-			<iframe src="http://streambadge.com/twitch/mantequillas/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-			<iframe src="http://streambadge.com/twitch/srsbsnstv/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-			<iframe src="http://streambadge.com/twitch/SeriousDubs/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-			<iframe src="http://streambadge.com/twitch/strifetalk/" style="border:none;height:4em;width:100%" scrolling="no"></iframe>
-        </div>
+<?php
+
+$streamerJsonArray = file_get_contents('http://streamapp.srsbsns.tv/Saves/streamers.json');
+$streamers = json_decode($streamerJsonArray);
+?>
+
+<div>
+    <h2>Streamers</h2>
+<div>
+<?php
+foreach($streamers as $streamer) {
+    if($streamer->stream != null) {
+?>
+<div>
+    <div style="display:block;float:left">
+        <img width="60" height="60" src="<?php echo $streamer->stream->channel->logo; ?>" />
+    </div>
+    <div style="display:block;float:left">
+<?php
+        echo "<a href='".$streamer->stream->channel->url."'>".$streamer->stream->channel->display_name. "</a><br />";
+        echo "Playing ".$streamer->stream->game."<br />";
+        echo $streamer->stream->viewers." viewers";
+?>
+    </div>
+</div>
+<?php
+    }
+}
+?>
+
+</div>
+</div>
