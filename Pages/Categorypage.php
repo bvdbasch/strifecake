fc4d6f14067c1f118ff4c1498ae06b4025e6ae5e
Roel Bakker <roelbaka@gmail.com>
2014-08-24 23:15:51 +0200

    changed views for categories and main page

--- Pages/Categorypage.php
+++ Pages/Categorypage.php
@@ -1,7 +1,7 @@
 <?php 
     if (has_posts()) {
         while (posts()) {
-                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                include(theme_path()."Partials/CategoryBlockHTML.php");
         }
     }
 ?>
---
+++ Partials/CategoryBlockHTML.php
@@ -0,0 +1,28 @@
+<article>
+ <div id="ArticlePreview">
+     <!-- Preview Image -->
+     <img class="frontpage" src="<?php echo article_custom_field("thumb",theme_url('Resources/IMG/fallback.png'));?>">
+    
+     <header>
+        <!-- Article title -->
+        <h1><a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"><?php echo article_title(); ?></a></h1>
+
+        <!-- Article subtitle -->
+        <h2><em>Posted by: <?php echo article_author();?> on <time datetime="<?php echo date(DATE_W3C, article_time());?>"><?php echo article_date();?></time></em></h2>
+     </header>
+
+     <!-- Article description -->
+     <p>
+        <?php
+            $maxDiscCharacters = 100;
+            $countedDiscription = strlen(article_description());
+
+            if($countedDiscription > $maxDiscCharacters+1){
+                echo substr(article_description(),0,$maxDiscCharacters)." ...";
+            }else{
+                echo article_description();
+            }
+        ?>
+    </p>
+</div>
+</article>
