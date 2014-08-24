ff3d698318ef2f75fe754d7a4ab325197896fdd6
bvdbasch <bvdbasch@outlook.com>
2014-08-24 21:40:01 +0200

    Cleaned up code a bit
    Improved readability :)

--- Partials/FrontpageCategoryBlockHTML.php
+++ Partials/FrontpageCategoryBlockHTML.php
@@ -1,6 +1,5 @@
 <article>
  <div id="ArticlePreview">
-     
      <!-- Preview Image -->
      <img class="frontpage" src="<?php echo article_custom_field("thumb",theme_url('Resources/IMG/fallback.png'));?>">
     
@@ -10,18 +9,9 @@
 
         <!-- Article subtitle -->
         <h2><em>Posted by: <?php echo article_author();?> on <time datetime="<?php echo date(DATE_W3C, article_time());?>"><?php echo article_date();?></time></em></h2>
-
-        <!-- End article title -->
+     </header>
     
-    </header>
-    
-    <!-- Article description -->
-
-    <p><?php echo substr(article_description(),0,100); ?><a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"> ...</a></p>
-     
-     
-     
-
-    <!-- End article description -->
+     <!-- Article description -->
+     <p><?php echo substr(article_description(),0,100); ?><a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"> ...</a></p>
 </div>
 </article>
