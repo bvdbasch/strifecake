8e2077db7f3ffbaa21801a5855aa841a45ec44d9
Roel Bakker <roelbaka@gmail.com>
2014-08-24 22:16:43 +0200

    added if no streamers sad message

--- Partials/Streamers.php
+++ Partials/Streamers.php
@@ -1,5 +1,5 @@
 <?php
-
+//read JsonArray from page and decode it to array
 $streamerJsonArray = file_get_contents('http://streamapp.srsbsns.tv/Saves/streamers.json');
 $streamers = json_decode($streamerJsonArray);
 ?>
@@ -8,23 +8,31 @@ $streamers = json_decode($streamerJsonArray);
     <h2>Streamers</h2>
 <div>
 <?php
+$aantalStreamers = 0;
 foreach($streamers as $streamer) {
     if($streamer->stream != null) {
 ?>
 <div>
     <div style="display:block;float:left">
-        <img width="60" height="60" src="<?php echo $streamer->stream->channel->logo; ?>" />
+        <img width="50" height="50" src="<?php echo $streamer->stream->channel->logo; ?>" />
     </div>
-    <div style="display:block;float:left">
+    <div style="display:block;float:left; padding-left:5px">
 <?php
-        echo "<a href='".$streamer->stream->channel->url."'>".$streamer->stream->channel->display_name. "</a><br />";
+        echo "<a href='".$streamer->stream->channel->url."'><strong>".$streamer->stream->channel->display_name. "</strong></a><br />";
         echo "Playing ".$streamer->stream->game."<br />";
         echo $streamer->stream->viewers." viewers";
 ?>
     </div>
 </div>
 <?php
+        $aantalStreamers++;
+?>
+    <br clear="both" />
+<?php
     }
+}
+if ($aantalStreamers == 0) {
+    echo "There are currently no streamers online :( ";
 }
 ?>
 
