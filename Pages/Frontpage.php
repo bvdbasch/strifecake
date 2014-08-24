70153041448b67afc364d9140a30d724ae996f49
Roel Bakker <roelbaka@gmail.com>
2014-08-24 22:59:15 +0200

    maximum 3 articles on front page

--- Pages/Frontpage.php
+++ Pages/Frontpage.php
@@ -5,53 +5,60 @@
     <div id="CategoryLeft">
         <div id="CategoryHeader">NEWS</div> <!-- Header -->    
         <div id="CatGreen"></div>           <!-- Accent -->
-    
+
         <!-- Content -->
         <div id="CategoryList">
         <?php 
             if (has_posts()) {
-                while (posts()) {
-            if ( article_category() == 'news') { 
-                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                $amountOfPosts=0;
+                while (posts() && $amountOfPosts < 3) {
+                    if ( article_category() == 'news') { 
+                        include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
                     }
+                    $amountOfPosts++;
                 }
             }
         ?>
         </div>
     </div>
-    
+
     <!-- LEARN CATEGORY-->
     <div id="CategoryRight">
         <div id="CategoryHeader">LEARN</div>    <!-- Header -->    
         <div id="CatOrange"></div>              <!-- Accent -->
-    
+
         <!-- Content -->
         <div id="CategoryList">
         <?php 
             if (has_posts()) {
-                while (posts()) {
-            if ( article_category() == 'news') { 
-                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                $amountOfPosts=0;
+                while (posts() && $amountOfPosts < 3) {
+                    if ( article_category() == 'news') { 
+                        include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
                     }
+                    $amountOfPosts++;
                 }
             }
         ?>
         </div>
     </div>
-    
+<br clear="both" />
+
     <!-- COMPETITIVE CATEGORY-->
     <div id="CategoryLeft">
         <div id="CategoryHeader">COMPETITIVE</div>  <!-- Header -->    
         <div id="CatPurple"></div>                  <!-- Accent -->
-    
+
         <!-- Content -->
         <div id="CategoryList">
         <?php 
             if (has_posts()) {
-                while (posts()) {
-            if ( article_category() == 'news') { 
-                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                $amountOfPosts=0;
+                while (posts() && $amountOfPosts < 3) {
+                    if ( article_category() == 'news') { 
+                        include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
                     }
+                    $amountOfPosts++;
                 }
             }
         ?>
@@ -62,34 +69,39 @@
     <div id="CategoryRight">
         <div id="CategoryHeader">MEDIA</div>   <!-- Header -->    
         <div id="CatRed"></div>                <!-- Accent -->
-    
+
         <!-- Content -->
         <div id="CategoryList">
         <?php 
             if (has_posts()) {
-                while (posts()) {
-            if ( article_category() == 'news') { 
-                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                $amountOfPosts = 0;
+                while (posts() && $amountOfPosts < 3) {
+                    if ( article_category() == 'news') { 
+                        include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
                     }
+                $amountOfPosts++;
                 }
             }
         ?>
         </div>
     </div>
-    
+
+<br clear="both" />
     <!-- BLOG CATEGORY-->
     <div id="CategoryLeft">
         <div id="CategoryHeader">BLOG</div>   <!-- Header -->    
         <div id="CatGray"></div>              <!-- Accent -->
-    
+
         <!-- Content -->
         <div id="CategoryList">
         <?php 
             if (has_posts()) {
-                while (posts()) {
-            if ( article_category() == 'news') { 
-                include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
+                $amountOfPosts = 0;
+                while (posts() && $amountOfPosts < 3) {
+                    if ( article_category() == 'news') { 
+                        include(theme_path()."Partials/FrontpageCategoryBlockHTML.php");
                     }
+                    $amountOfPosts++;
                 }
             }
         ?>
