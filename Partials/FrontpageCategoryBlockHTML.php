b79bdfc423edd6e054dae41458db70c7dd020030
Roel Bakker <roelbaka@gmail.com>
2014-08-24 23:10:26 +0200

    added ... only if article longer discription longer than max chars

--- Partials/FrontpageCategoryBlockHTML.php
+++ Partials/FrontpageCategoryBlockHTML.php
@@ -10,8 +10,19 @@
         <!-- Article subtitle -->
         <h2><em>Posted by: <?php echo article_author();?> on <time datetime="<?php echo date(DATE_W3C, article_time());?>"><?php echo article_date();?></time></em></h2>
      </header>
-    
+
      <!-- Article description -->
-     <p><?php echo substr(article_description(),0,100); ?><a href="<?php echo article_url(); ?>" title="<?php echo article_title(); ?>"> ...</a></p>
+     <p>
+        <?php
+            $maxDiscCharacters = 100;
+            $countedDiscription = strlen(article_description());
+
+            if($countedDiscription > $maxDiscCharacters+1){
+                echo substr(article_description(),0,$maxDiscCharacters)." ...";
+            }else{
+                echo article_description();
+            }
+        ?>
+    </p>
 </div>
 </article>
