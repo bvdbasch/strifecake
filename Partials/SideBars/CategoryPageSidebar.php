6ee8de558d7c5db7cf59be2b2fc723f6f50c885c
bvdbasch <bvdbasch@outlook.com>
2014-08-25 14:53:26 +0200

    Added learn widget to sidebar
    Added more elements to sidebar :+1:

--- Partials/SideBars/CategoryPageSidebar.php
+++ Partials/SideBars/CategoryPageSidebar.php
@@ -1,1 +1,2 @@
+<?php include(theme_path()."Partials/Widgets/Learn.php");?>
 <?php include(theme_path()."Partials/Widgets/Streamers.php");?>
---
+++ Partials/Widgets/Learn.php
@@ -0,0 +1,7 @@
+<div id= "SidebarWrapper">
+    <div id="SidebarHeader">New to Strife?</div>
+    
+    <div id="SidebarContent">
+        <center><img src="<?php echo theme_url('/Resources/IMG/button_learn.png') ?>" /></center>
+    </div>
+</div>
