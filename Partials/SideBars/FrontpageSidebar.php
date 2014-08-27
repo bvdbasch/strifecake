877ff7cf210f79110ef7c135a03e126580638f09
Roel Bakker <roelbaka@gmail.com>
2014-08-27 21:38:33 +0200

    add reddit to sidebar

--- Partials/SideBars/FrontpageSidebar.php
+++ Partials/SideBars/FrontpageSidebar.php
@@ -1,2 +1,3 @@
 <?php include(theme_path()."Partials/Widgets/Learn.php");?>
 <?php include(theme_path()."Partials/Widgets/Streamers.php");?>
+<?php include(theme_path()."Partials/Widgets/RedditFeed.php");?>
