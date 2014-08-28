568d725b982662408c4916d0b3a06515777f3648
Roel Bakker <roelbaka@gmail.com>
2014-08-29 00:40:34 +0200

    google analytics added

--- Partials/Footer.php
+++ Partials/Footer.php
@@ -1,23 +1,29 @@
-    <!-- FOOTER STARTS HERE -->
-    <div id="FooterWrapper">
-        <div id="FooterInner">
-            <img src="<?php echo theme_url('/Resources/IMG/sbtv.png'); ?>" class="footer" />
-            <div id="FooterDivider"></div>
-            <ul>
-                <li class="Footer">About</li>
-                <li class="Footer">Team Kaizen</li>
-                <li class="Footer">Streamers</li>
-                <li class="Footer">Twitter</li>
-                <li class="Footer">Facebook</li>
-            </ul>
+<div id="FooterWrapper">
+    <div id="FooterInner">
+        <img src="<?php echo theme_url('/Resources/IMG/sbtv.png'); ?>" class="footer" />
+        <div id="FooterDivider"></div>
+        <ul>
+            <li class="Footer">About</li>
+            <li class="Footer">Team Kaizen</li>
+            <li class="Footer">Streamers</li>
+            <li class="Footer">Twitter</li>
+            <li class="Footer">Facebook</li>
+        </ul>
 
-            <br clear="both"/>
+        <br clear="both"/>
 
-            <center>
-                <em><?php echo site_name() ?></em>
-            </center>
-        </div>
+        <center>
+            <em><?php echo site_name() ?></em>
+        </center>
     </div>
-    <!-- END FOOTER -->
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-52814889-1', 'auto');
+  ga('send', 'pageview');
 
+</script>
