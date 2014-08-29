65c0754b2fcd2d6c99d12dd1ea2782876f1232ee
bvdbasch <bvdbasch@outlook.com>
2014-08-29 15:00:04 +0200

    Updated slider
    Actual content. Sorry crouching dudes...

--- Partials/Widgets/Slider.php
+++ Partials/Widgets/Slider.php
@@ -10,9 +10,8 @@
 <!--Slider data here, styling is in CSS -->
 <div id="Slider">
     <ul class="bxslider">
-        <li><a href="http://www.google.nl"><img src="<?php echo theme_url('/Resources/IMG/Slider/strife-school-slider.png') ?>" /></a></li>
-        <li><a href="http://www.google.nl"><img src="<?php echo theme_url('/Resources/IMG/Slider/test.jpg') ?>" /></a></li>
-        <li><a href="http://www.google.nl"><img src="<?php echo theme_url('/Resources/IMG/Slider/strife-school-slider.png') ?>" /></a></li>
-        <li><a href="http://www.google.nl"><img src="<?php echo theme_url('/Resources/IMG/Slider/test.jpg') ?>" /></a></li>
+        <li><a href="<?php echo base_url('/posts/strife-school-5-improvement'); ?>"><img src="<?php echo theme_url('/Resources/IMG/Slider/strife-school-slider.png') ?>" /></a></li>
+        <li><a href="<?php echo base_url('/posts/strife-synergy-3-blitz-and-vip'); ?>"><img src="<?php echo theme_url('/Resources/IMG/Slider/strife-synergy-slider.png') ?>" /></a></li>
+        <li><a href="<?php echo base_url('posts/life-outside-of-strife-s2-vicious'); ?>"><img src="<?php echo theme_url('/Resources/IMG/Slider/los-slider.png') ?>" /></a></li>
     </ul>
 </div>
