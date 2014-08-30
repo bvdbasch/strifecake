e7cedcf7fe21eaab6dfa7203c6f7cd665b321a00
bvdbasch <bvdbasch@outlook.com>
2014-08-30 15:19:26 +0200

    Updated slider with more articles
    Now includes PAX

--- Partials/Widgets/Slider.php
+++ Partials/Widgets/Slider.php
@@ -10,6 +10,7 @@
 <!--Slider data here, styling is in CSS -->
 <div id="Slider">
     <ul class="bxslider">
+        <li><a href="<?php echo base_url('/posts/sb-goes-to-pax-day-1'); ?>"><img src="<?php echo theme_url('/Resources/IMG/Slider/pax-day1-slider.png') ?>" /></a></li>
         <li><a href="<?php echo base_url('/posts/strife-school-5-improvement'); ?>"><img src="<?php echo theme_url('/Resources/IMG/Slider/strife-school-slider.png') ?>" /></a></li>
         <li><a href="<?php echo base_url('/posts/strife-synergy-3-blitz-and-vip'); ?>"><img src="<?php echo theme_url('/Resources/IMG/Slider/strife-synergy-slider.png') ?>" /></a></li>
         <li><a href="<?php echo base_url('posts/life-outside-of-strife-s2-vicious'); ?>"><img src="<?php echo theme_url('/Resources/IMG/Slider/los-slider.png') ?>" /></a></li>
