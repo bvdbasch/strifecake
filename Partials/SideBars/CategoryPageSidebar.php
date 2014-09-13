facec59628679ef87ebe3d9107c0473db39a662a
bvdbasch <vexxie@Vexxies-Mac-Pro.local>
2014-09-13 16:40:21 +0200

    Disabled streamer widget in sidebar
    Streamer widget is causing issues, commented out include and logged
    issue on github

--- Partials/SideBars/CategoryPageSidebar.php
+++ Partials/SideBars/CategoryPageSidebar.php
@@ -1,2 +1,2 @@
 <?php include(theme_path()."Partials/Widgets/Learn.php");?>
-<?php include(theme_path()."Partials/Widgets/Streamers.php");?>
+<?php //include(theme_path()."Partials/Widgets/Streamers.php");?>
--- Partials/SideBars/FrontpageSidebar.php
+++ Partials/SideBars/FrontpageSidebar.php
@@ -1,3 +1,3 @@
 <?php include(theme_path()."Partials/Widgets/Learn.php");?>
-<?php include(theme_path()."Partials/Widgets/Streamers.php");?>
+<?php //include(theme_path()."Partials/Widgets/Streamers.php");?>
 <?php include(theme_path()."Partials/Widgets/RedditFeed.php");?>
